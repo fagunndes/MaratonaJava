7b14f1b878e563db14fe43cccac1180178e5f991
fagunndes <fagunndes@gmail.com>
2019-11-01 23:58:27 -0300

    Adicionando a classe PrimitivoInt

---
+++ PrimitivoInt.java
@@ -0,0 +1,9 @@
+public class PrimitivoInt{
+  public static void main(String[] args){
+    int numero1 = 10;
+    int numero2 = 52;
+    int soma    = numero1 + numero2;
+    
+    System.out.println("Soma: " + soma);
+  }
+}
